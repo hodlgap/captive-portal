09496718d6ce29893c7d96718c5396f5823fc267
Kcrong <iam@devkcr.org>
2023-10-02 21:21:17 +0900

    Update

--- serve/ndslog/ndslog_log.php
+++
@@ -1,3 +0,0 @@
-<?php exit(0); ?>
-2023-09-30 15:01:01, fas-aes-https, gwgwgw%20Node%3a88366c31a26c%20, asdf, asdf@asdf, 192.168.1.100, f8:e4:3b:d6:1e:43, cpi_url, br-lan, Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36, http://status.client/
-2023-09-30 15:09:57, fas-aes-https, gwgwgw%20Node%3a88366c31a26c%20, Hyunwoo, asdf@asdf.asdf, 192.168.1.100, f8:e4:3b:d6:1e:43, cpi_url, br-lan, Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36, http://status.client/
