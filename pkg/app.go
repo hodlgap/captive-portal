7787f0fac9f8c756e6e5afb4f62cf1ee9eb6715f
Kcrong <iam@devkcr.org>
2023-10-04 22:46:49 +0900

    Update

--- pkg/app.go
+++ pkg/app.go
@@ -22,6 +22,9 @@ func NewApp(c config.Config, nr *newrelic.Application, authProvider auth.Provide
 	app.Use(middleware.Recover())
 	app.Use(nrecho.Middleware(nr))
 
+	app.GET("/", func(c echo.Context) error {
+		return c.String(200, "https://resume.devkcr.org")
+	})
 	app.GET(captiveportal.AuthHandlerURL, captiveportal.NewAuthHandler(c.Openwrt.EncryptionKey, authProvider, db))
 	app.POST(captiveportal.AuthGetHandlerURL, captiveportal.NewAuthGetHandler(authProvider))
 
